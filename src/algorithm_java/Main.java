014bc8892684f99fa0de919efa269d7d8f0bced4
jckim <whd4028@gmail.com>
2022-02-14 21:39:25 +0900

    기본 Main 클래스

---
+++ src/algorithm_java/Main.java
@@ -0,0 +1,15 @@
+package algorithm_java;
+
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args){
+		//입력단 start
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt(); // 입력받을 K 값
+		
+	}
+
+}
