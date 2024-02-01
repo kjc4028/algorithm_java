f5c637ce6fb270ba336ecf6e6cb70666876edb6e
jckim <whd4028@gmail.com>
2024-02-02 02:42:26 +0900

    N과 M(4) - dfs

---
+++ src/algorithm_java/BOJ15652.java
@@ -0,0 +1,38 @@
+package algorithm_java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15652 {
+    static int N;
+    static int M;
+    static int[] arr;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(0,0);
+    }
+
+    public static void dfs(int depth, int from){
+        if(depth == M){
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = from; i < N; i++) {
+            arr[depth] = i+1;
+            dfs(depth+1, i);
+        }
+    }
+}
